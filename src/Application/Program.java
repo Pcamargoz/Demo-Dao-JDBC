77fb10bd53a1813a85f7c29fc3f5ed5cbbdf25b5
Pedro Camargo <pedro01cesarsanto@gmail.com>
2025-11-29 19:06:50 -0300

    Department entity class

---
+++ src/Application/Program.java
@@ -0,0 +1,10 @@
+package Application;
+
+import ModelEntities.Department;
+
+public class Program {
+    static void main() {
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+}
---
+++ src/ModelEntities/Department.java
@@ -0,0 +1,58 @@
+package ModelEntities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private Integer id;
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+    // para ser comparado com objetos e nao com ponteiro
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Department(){
+
+    }
+
+}
